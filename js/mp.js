4f68e9d0418239f4ea64218b57f4c081872b4dc0
Kimminsu <kmsa0202@gmail.com>
2024-04-17 16:41:51 +0900

    240417 수업종료

--- js/mp.js
+++ js/mp.js
@@ -1,3 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(){
+  // JavaScript 추가
+  document.getElementById('signup-form').addEventListener('submit', function(event) {
+      event.preventDefault(); // 기본 제출 동작 방지
+    
+      var username = document.getElementById('username').value;
+      var password = document.getElementById('password').value;
+      var password = document.getElementById('email').value;
+    
+    
+      // 아이디와 비밀번호가 빈 값인지 확인
+      if (username.trim() === '' || password.trim() === '' || email.trim()==='') {
+        alert('아이디, 비밀번호 혹은 이메일을 확인하세요.');
+        return;
+      }
+    
+      // 아이디와 비밀번호의 유효성 검사 (이 부분은 필요에 따라서 추가해주세요)
+      // 예: 아이디의 길이, 비밀번호의 복잡성 등
+      // 예를 들어, 아이디가 6자 이상 12자 이하인지 확인하고 싶으면 다음과 같이 추가할 수 있습니다.
+    
+      alert('제출 완료!');
+    
+    });
+  });
+
+document.addEventListener('DOMContentLoaded', function(){
 // JavaScript 추가
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault(); // 기본 제출 동작 방지
@@ -27,9 +53,30 @@ document.getElementById('login-form').addEventListener('submit', function(event)
   
     // 여기서 다음 페이지로 이동하는 등의 작업을 수행합니다.
   });
-  
-  // Main 버튼 클릭 시 메인 페이지로 이동하는 이벤트 처리
-  document.querySelector('button[type="button"]').addEventListener('click', function() {
-    window.location.href = 'mp_main.html'; // 메인 페이지로 이동
-  });
-  
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Main 버튼 클릭 시 메인 페이지로 이동하는 이벤트 처리
+    var mainButton = document.getElementById("main");
+    if (mainButton) {
+        mainButton.addEventListener("click", function() {
+            window.location.href = 'mp_main.html'; // 메인 페이지로 이동
+        });
+    }
+
+    // SignUp 버튼 클릭 시 회원가입 페이지로 이동하는 이벤트 처리
+    var signupButton = document.getElementById("signup");
+    if (signupButton) {
+        signupButton.addEventListener("click", function() {
+            window.location.href = 'mp_signup.html';
+        });
+    }
+
+      // Login 버튼 클릭 시 로그인 페이지로 이동하는 이벤트 처리
+      var loginButton = document.getElementById("login");
+      if (loginButton) {
+          loginButton.addEventListener("click", function() {
+              window.location.href = 'mp_login.html';
+          });
+      }
+});
